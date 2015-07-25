dc27600595acd23c820063361dfc867ce2110bb4
Will Holcomb <will@dhappy.org>
2015-07-25 15:55:27 -0400

    renaming

--- fb16.js
+++ fb16.js
@@ -4,14 +4,18 @@ function decodeUTF16LE( binaryStr ) {
         cp.push(
             binaryStr.charCodeAt(i) |
                 ( binaryStr.charCodeAt(i+1) << 8 )
-        );
+        )
     }
 
-    return String.fromCharCode.apply( String, cp );
+    return String.fromCharCode.apply( String, cp )
 }
 
 var base64decode = atob; //In chrome and firefox, atob is a native method available for base64 decoding
 
-var base64 = "VABlAHMAdABpAG4AZwA";
-var binaryStr = base64decode(base64);
-var result = decodeUTF16LE(binaryStr);
+$(function() {
+    var base64 = "VABlAHMAdABpAG4AZwA"
+    var binaryStr = atoixs(base64)
+    var result = decodeUTF16LE(binaryStr)
+    $('#base64').text(base64)
+    $('#UTF16LE').text(result)
+})
