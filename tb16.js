0b9623c6b40652d082dc1d1750e5f6dd078edaef
Will Holcomb <will@dhappy.org>
2015-07-26 18:54:39 -0400

    An honest tale speeds best being plainly told. 		-- William Shakespeare, "Henry VI"

--- tb16.js
+++ tb16.js
@@ -1,19 +1,24 @@
-function decodeUTF16LE( binaryStr ) {
-    var cp = [];
-    for( var i = 0; i < binaryStr.length; i+=2) {
-        cp.push(
-            binaryStr.charCodeAt(i) |
-                ( binaryStr.charCodeAt(i+1) << 8 )
-        )
+String.prototype.each_slice = function(size, callback) {
+    var out = []
+    for(var i = 0, l = this.length; i < l; i += size) {
+        out.push(callback.call(this, this.slice(i, i + size)))
     }
-
-    return String.fromCharCode.apply( String, cp )
+    return out
 }
 
 function encodeTB16( data ) {
     var out = ''
     for(var i = 0; i < data.length; i += 8) {
-        out += String.fromCharCode(parseInt(data.slice(i, i + 8), 2))
+        out += String.fromCharCode(parseInt(data.slice(i, i + 7), 2))
+    }
+
+    return out
+}
+
+function encodeUTF16( data ) {
+    var out = ''
+    for(var i = 0; i < data.length; i += 16) {
+        out += String.fromCharCode(parseInt(data.slice(i, i + 15), 2))
     }
 
     return out
@@ -23,24 +28,24 @@ var base64decode = atob; //In chrome and firefox, atob is a native method availa
 
 $(function() {
     var binary = ''
-    for(var i = 1; i <= 128; i++) {
+    for(var i = 1; i <= 128 * 128; i++) {
         binary += Math.random() >= .5 ? '1' : '0'
     }
-    var vars = {}
-    vars.merge({
-        hex: parseInt(binary, 2).toString(16),
-        base64: "VABlAHMAdABpAG4AZwA",
-        binary: atob(vars['base64']),
-        UTF16LE: decodeUTF16LE(binary),
-        tb16: encodeTB16(binary)
-    })
-    for(type in vars) {
-        $('#' + type).text(vars[type])
+    var vars = {
+        binary: (function() {
+            return binary.each_slice(1, function(bit) {
+                return bit
+            }).join(String.fromCharCode(parseInt('AD', 16))) // soft hyphens
+        })(),
+        hex: (function() {
+            return binary.each_slice(64, function(bits) {
+                return parseInt(bits, 2).toString(16)
+            }).join(String.fromCharCode(parseInt('AD', 16))) // soft hyphens
+        })(),
+        tb16: encodeTB16(binary),
+        utf16: encodeUTF16(binary)
     }
-
-    var $ol = $('<ol/>')
-    $('body').append($ol)
-    for(var i = 0; i < parseInt('FFFF', 16); i++) {
-        $ol.append($('<li/>').text(i.toString(16) + ': ' + String.fromCharCode(i)))
+    for(type in vars) {
+        $('#' + type).text('(ℓ:' + vars[type].length + '): ' + vars[type])
     }
 })
